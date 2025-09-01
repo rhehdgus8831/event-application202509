5f9db0d25e7c38c4e3142ec9462b4ed9e8b058ec
rhehdgus8831 <rhehdgus8831@naver.com>
2025-09-01 11:19:37 +0900

    [FE] 서버에서 이벤트 목록 가져와서 렌더링

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MainNavigation from '../components/MainNavigation.jsx';
 
 const EventPage = () => {
+
+    const [eventList, setEventList] = useState([]);
+
+    // API CALL
+    const fetchEvents = async () => {
+        const resopnse = await fetch('http://localhost:9000/api/events');
+        const data = await resopnse.json();
+        console.log(data);
+
+        setEventList(data);
+    };
+
+
+
+    useEffect(() => {
+        fetchEvents();
+    },[])
+
+
     return (
         <>
          <h1>Event Page</h1>
+            <ul>
+                {eventList.map(event => <li key={event.id}>{event.title}</li>)}
+            </ul>
         </>
     );
 };
