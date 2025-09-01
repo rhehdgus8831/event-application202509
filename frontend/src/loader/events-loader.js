6f8b1711f901295868c681a24a2a934f0b58db14
rhehdgus8831 <rhehdgus8831@naver.com>
2025-09-01 12:32:59 +0900

    [FE] loader 아웃소싱

---
+++ frontend/src/loader/events-loader.js
@@ -0,0 +1,10 @@
+
+export const eventsListLoader = async () => {
+    // console.log(`event loader call!`);
+    const response = await fetch('http://localhost:9000/api/events');
+
+    // loader가 리턴한 데이터는 라우팅된 페이지와 그 하위 컴포넌트에서 언제든 사용 가능하다.
+    // loader는 fetch 결과를 바로 리턴하는 겨우 알아서 json을 추출한다.
+    // return await response.json();
+    return response;
+}
